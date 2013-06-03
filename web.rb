fed518aaa576c276700ab2208093951f4cba379a
Mikko Tamminen <pahanaama@gmail.com>
2013-06-03 23:28:28 +0300

    kirjautuminen

--- web.rb
+++ web.rb
@@ -9,21 +9,65 @@ class App < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  get '/' do
-    "hello"
+  helpers do
+    def kirjautunut?
+      !session[:kayttaja].nil?
+    end
+
+    def kirjautunut_kayttaja
+      DB.fetch("SELECT id, username FROM users WHERE id = ?", session[:kayttaja]).first
+    end
+  end
+
+  before do
+    unless kirjautunut? or request.path_info =~ /^\/loginregister/ or request.path_info =~ /^\/login/ or request.path_info =~ /^\/register/
+      redirect '/loginregister'
+    end
   end
 
   get '/uploadtest' do
     haml :uploadfile
   end
 
-  post '/uploadtest' do
-    unless DB.table_exists?(:pictures)
-      DB.create_table :pictures do
-        column :name, :text
-        column :filename, :text
-      end
+  get '/loginregister' do
+    haml :loginregister
+  end
+
+  post '/register' do
+    username = params[:username]
+    password = params[:password]
+    users = DB[:users]
+    if !DB[:users].where(:username => username).empty?
+      return "error, username already exists"
+    else
+      users << {:username => username, :password => password}
+      "Successfully added user " + username +  " <br>with password " + password
     end
+  end
+
+  post '/login' do
+    ret = "Available users and passwords: <br>"
+    for user in DB[:users] do
+       ret += user[:username] + ":" + user[:password] + "<br>"
+    end
+
+    user = DB[:users].where('username = ?', params[:username])
+    p user
+    if user.empty?
+      return ret + "Username not found!"
+    end
+
+    user = DB[:users].where('username = ? and password = ?', params[:username], params[:password])
+    if user.empty?
+      return ret + "supplied password incorrect!"
+    end
+
+    session[:kayttaja] = user.first[:id]
+
+    return ret + "Successfully logged in."
+  end
+
+  post '/uploadtest' do
 
     filename = 'uploads/' + params[:name]
     picname = params[:name]
@@ -39,37 +83,46 @@ class App < Sinatra::Base
       end
   end
 
-  get '/t3' do
-    @name = 'pelle'
-    erb :testview, name: @name
-  end
-
-  post '/testpost' do
-    "what you inputtin \"#{params[:name]}\" fo anyway?"
-  end
-
-  get '/test' do
-    unless DB.table_exists?(:items)
-      DB.create_table :items do
-        column :name, :text
-        column :price, :float
-      end
-    end
-
-    items = DB[:items]
-    items << {:name => 'abc', :price => rand * 100}
-    items << {:name => 'def', :price => rand * 100}
-    items << {:name => 'ghi', :price => rand * 100}
-
-    puts "Item Count = #{items.count}"
-
-    items.reverse_order(:price).each do |item|
-      p item
-    end
-
-    puts "The average price is: #{items.avg(:price)}"
-  end
-
 end
 
+def initDB
+
+  unless DB.table_exists?(:users)
+    DB.create_table :users do
+      primary_key :id
+      String :username, :unique => true
+      String :password
+    end
+  end
+
+  unless DB.table_exists?(:pictures)
+    DB.create_table :pictures do
+      primary_key :id
+      String :name
+      String :path
+      foreign_key :owner, :users
+      TrueClass :public, :default => true
+    end
+  end
+
+  unless DB.table_exists?(:categorizations)
+    DB.create_table :categorizations do
+      primary_key :id
+      String :category_name
+      foreign_key :picture_id, :pictures
+    end
+  end
+
+  unless DB.table_exists?(:comments)
+    DB.create_table :comments do
+      primary_key :id
+      String :text
+      foreign_key :commenter, :users
+      foreign_key :picture_id, :pictures
+    end
+  end
+end
+
+initDB
 App.run!
+
