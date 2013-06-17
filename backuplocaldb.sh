048a8c99ff3e6562bc608aa0eebaffa8f0326209
Mikko Tamminen <pahanaama@gmail.com>
2013-06-18 01:37:02 +0300

    edited the backupscript

--- backuplocaldb.sh
+++ backuplocaldb.sh
@@ -1,5 +1,7 @@
 PGPASSWORD=mikko pg_dump -Fc --no-acl --no-owner -h localhost -U postgres mydb > mydb.dump
-echo "Paste the url where the backup resides"
+echo "local db dumped to ./mydb.dump"
+echo "Upload the file to the internets and"
+echo "paste the url where the backup resides"
 read BACKUP
 heroku pgbackups:restore HEROKU_POSTGRESQL_BLUE_URL $BACKUP --confirm glacial-savannah-7038
 
